a1b7f6db50bbc217c646919893fd26035eb9f26b
cforth <cforth@163.com>
2019-02-13 13:24:30 +0800

    改进文件分割合并测试

--- CryptoFactory/tests/test_file.py
+++ CryptoFactory/tests/test_file.py
@@ -6,15 +6,19 @@ from CryptoFactory.File import *
 class TestFile(unittest.TestCase):
     def test_file_split_merge(self):
         md5_value = get_file_md5('./testdata/test.mp4')
-        file_split('./testdata/test.mp4', './testdata/test_split.mp4', 100*1024*1024)
-        file_merge('./testdata/test_split.mp4', './testdata/test_merge.mp4', 5)
+        file_split('./testdata/test.mp4', './testdata/test_split.mp4', 100 * 1024 * 1024)
+
+        sub_files_count = 0
+        for file_path in os.listdir('./testdata/'):
+            if 'test_split.mp4' in file_path:
+                sub_files_count += 1
+
+        file_merge('./testdata/test_split.mp4', './testdata/test_merge.mp4', sub_files_count)
         merge_md5_value = get_file_md5('./testdata/test_merge.mp4')
         self.assertEqual(md5_value, merge_md5_value)
-        os.remove('./testdata/test_split.mp4.1')
-        os.remove('./testdata/test_split.mp4.2')
-        os.remove('./testdata/test_split.mp4.3')
-        os.remove('./testdata/test_split.mp4.4')
-        os.remove('./testdata/test_split.mp4.5')
+        for num in range(1, sub_files_count + 1):
+            os.remove('./testdata/test_split.mp4.' + str(num))
+
         os.remove('./testdata/test_merge.mp4')
 
 
