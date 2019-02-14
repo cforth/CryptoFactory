88d4192b4c5888bf5e7de54a7c552037f48b5d80
cforth <cforth@163.com>
2019-02-14 16:11:08 +0800

    增加了加密数据库操作

--- CryptoFactory/ORM.py
+++ CryptoFactory/ORM.py
@@ -1,13 +1,26 @@
 import sqlite3
 import logging
 import os
+import base64
+import hashlib
+from CryptoFactory.Cipher import FileCrypto
 
 logging.basicConfig(level=logging.ERROR)
 
 
 # 数据库操作，负责连接、提交事务、断开数据库，返回操作的行数与结果列表
-def operate(db_name, execute_str, execute_args=None):
-    conn = sqlite3.connect(db_name + ".db")
+def operate(db_name, execute_str, execute_args=None, encrypt=False, db_password=""):
+    iv_str = ""
+    if encrypt:
+        md5 = hashlib.md5()
+        md5.update((db_password + 'salt').encode('utf-8'))
+        key = md5.digest()
+        iv_str = base64.b64encode(key).decode('utf-8')
+        if os.path.exists(db_name + ".db"):
+            FileCrypto(db_password, iv_str).decrypt(db_name + ".db", db_name + ".temp.db")
+        conn = sqlite3.connect(db_name + ".temp.db")
+    else:
+        conn = sqlite3.connect(db_name + ".db")
     try:
         cursor = conn.cursor()
         if execute_args:
@@ -22,6 +35,11 @@ def operate(db_name, execute_str, execute_args=None):
         raise e
     finally:
         conn.close()
+    if encrypt:
+        if os.path.exists(db_name + ".db"):
+            os.remove(db_name + ".db")
+        FileCrypto(db_password, iv_str).encrypt(db_name + ".temp.db", db_name + ".db")
+        os.remove(db_name + ".temp.db")
     return row_size, result
 
 
@@ -96,12 +114,12 @@ class Model(dict, metaclass=ModelMetaclass):
 
     # 返回是否存在该数据表
     @classmethod
-    def has_table(cls):
+    def has_table(cls, encrypt=False, db_password=""):
         if not os.path.exists(cls.__table__ + ".db"):
             logging.warning('DB file not exist!')
             return False
         execute_str = "select count(*) from sqlite_master where type=? and name=?"
-        row_size, result = operate(cls.__table__, execute_str, ("table", cls.__table__))
+        row_size, result = operate(cls.__table__, execute_str, ("table", cls.__table__), encrypt, db_password)
         if result[0][0]:
             return True
         else:
@@ -110,7 +128,7 @@ class Model(dict, metaclass=ModelMetaclass):
 
     # 新建数据表，默认数据表与数据库同名
     @classmethod
-    def new_table(cls):
+    def new_table(cls, encrypt=False, db_password=""):
         fields = []
         column_types = []
         primary = cls.__primary__
@@ -128,23 +146,23 @@ class Model(dict, metaclass=ModelMetaclass):
             sql += ' %s %s, ' % (fields[i], column_types[i])
         sql += ' %s %s)' % (fields[-1], column_types[-1])
         logging.info('SQL CREATE: %s' % sql)
-        operate(cls.__table__, sql)
+        operate(cls.__table__, sql, encrypt=encrypt, db_password=db_password)
 
     # 删除数据表
     @classmethod
-    def delete_table(cls):
+    def delete_table(cls, encrypt=False, db_password=""):
         sql = 'DROP TABLE %s' % cls.__table__
-        operate(cls.__table__, sql)
+        operate(cls.__table__, sql, encrypt=encrypt, db_password=db_password)
 
     # 查询数据库中指定列名和值的记录
     @classmethod
-    def find_all(cls, column_key, column_value):
+    def find_all(cls, column_key, column_value, encrypt=False, db_password=""):
         # 先将select的键名保存起来
         key_list = [k for k in cls.__mappings__]
         sql = 'select %s from %s where %s = ?' % (', '.join(['`%s`' % k for k in key_list]), cls.__table__, column_key)
         logging.info('SQL SELECT: %s' % sql)
         logging.info('ARGS: %s' % str(column_value))
-        row_size, result = operate(cls.__table__, sql, (column_value,))
+        row_size, result = operate(cls.__table__, sql, (column_value,), encrypt=encrypt, db_password=db_password)
         result_list = []
         if not result:
             return None
@@ -155,21 +173,21 @@ class Model(dict, metaclass=ModelMetaclass):
 
     # 查询数据库中是否存在指定列名和值的记录
     @classmethod
-    def has_item(cls, column_key, column_value):
-        result_list = cls.find_all(column_key, column_value)
+    def has_item(cls, column_key, column_value, encrypt=False, db_password=""):
+        result_list = cls.find_all(column_key, column_value, encrypt, db_password)
         return result_list[0] if result_list else None
 
     # 根据列名和值删除数据表中的数据
     @classmethod
-    def remove_all(cls, column_key, column_value):
+    def remove_all(cls, column_key, column_value, encrypt=False, db_password=""):
         sql = "delete from %s where %s = ?" % (cls.__table__, column_key)
         logging.info('SQL DELETE: %s' % sql)
         logging.info('ARGS: %s' % str(column_value))
-        row_size = operate(cls.__table__, sql, (column_value,))[0]
+        row_size = operate(cls.__table__, sql, (column_value,), encrypt, db_password)[0]
         return row_size
 
     # 插入一行数据到数据表
-    def save(self):
+    def save(self, encrypt=False, db_password=""):
         fields = []
         args = []
         for k, v in self.__mappings__.items():
@@ -179,11 +197,11 @@ class Model(dict, metaclass=ModelMetaclass):
         sql = 'insert into %s (%s) values (%s)' % (self.__table__, ','.join(fields), ','.join(['?' for i in args]))
         logging.info('SQL INSERT: %s' % sql)
         logging.info('ARGS: %s' % str(args))
-        row_size = operate(self.__table__, sql, tuple(args))[0]
+        row_size = operate(self.__table__, sql, tuple(args), encrypt, db_password)[0]
         return row_size
 
     # 根据列名和值修改数据表中的数据
-    def update_by(self, column_key, column_value):
+    def update_by(self, column_key, column_value, encrypt=False, db_password=""):
         fields = []
         args = []
         for k, v in self.__mappings__.items():
@@ -195,5 +213,5 @@ class Model(dict, metaclass=ModelMetaclass):
         args.append(column_value)
         logging.info('SQL UPDATE: %s' % sql)
         logging.info('ARGS: %s' % str(args))
-        row_size = operate(self.__table__, sql, tuple(args))[0]
+        row_size = operate(self.__table__, sql, tuple(args), encrypt, db_password)[0]
         return row_size
